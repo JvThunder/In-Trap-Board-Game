fe2a644380592e1d9b68dc2c90594c42098b0257
JvThunder <joshuaadrianc@gmail.com>
2023-01-22 16:08:05 +0800

    v0.1.3

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 if __name__ == "__main__":
     # Simulate a game
     from InTrap_env import InTrap, simulate_game, evaluate
-    from agents.heuristic_v1_agent import HeuristicAgent
+    from agents.heuristic_v1_agent import HeuristicAgent1
     env = InTrap()
 
     # To simulate 1 game using the code below 
     # simulate_game(env, HeuristicAgent, HeuristicAgent, verbose = True)
 
     # To check winning rate on 100 games using the code below
-    evaluate(env, HeuristicAgent, HeuristicAgent, N_GAMES = 10, verbose = False)
+    evaluate(env, HeuristicAgent1, HeuristicAgent1, N_GAMES = 10, verbose = False)
